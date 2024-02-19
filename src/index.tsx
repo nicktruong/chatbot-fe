950a9cec071f6b45420d0d4a330d10c87371a5f9
Nick <nick.truong.gos@gmail.com>
2024-02-19 21:06:24 +0700

    chore: set inter font

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,16 @@ import ReactDOM from 'react-dom/client';
 import App from './app';
 import reportWebVitals from './reportWebVitals';
 
+import 'styles/global.css';
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
