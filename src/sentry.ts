09631f20827076e509effa5d8a4f1829e09b8553
Nick <nick.truong.gos@gmail.com>
2024-02-20 09:19:30 +0700

    chore: setup sentry environment

--- src/sentry.ts
+++ src/sentry.ts
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/react';
 
 Sentry.init({
   dsn: process.env.REACT_APP_SENTRY_DSN,
+  environment: process.env.REACT_APP_SENTRY_ENV,
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration({
