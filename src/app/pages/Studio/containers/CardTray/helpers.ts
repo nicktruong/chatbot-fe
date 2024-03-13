04904e775dd63f1e200a1ff39ce0b346d366cdbd
Nick <nick.truong.gos@gmail.com>
2024-03-13 10:04:00 +0700

    fix: merge conflict

--- src/app/pages/Studio/containers/CardTray/helpers.ts
+++ src/app/pages/Studio/containers/CardTray/helpers.ts
@@ -1,14 +1,62 @@
-import { useAppDispatch, useAppSelector } from '@/hooks';
-import { closeCardTray, selectCardTrayOpen } from '@/store/studio';
+import { useQueryClient } from '@tanstack/react-query';
+
+import {
+  useAppDispatch,
+  useAppSelector,
+  useGetCardTypes,
+  useCreateCardMutation,
+} from '@/hooks';
+import {
+  selectNodeId,
+  closeCardTray,
+  selectCardTrayOpen,
+} from '@/store/studio';
+import { CardType } from '@/interfaces';
+import { queryKeys } from '@/constants';
+import { GroupTypeEnum } from '@/enums';
 
 export const usePrepareHook = () => {
   const dispatch = useAppDispatch();
+  const queryClient = useQueryClient();
 
+  const { mutate } = useCreateCardMutation({
+    onSuccess: (_, { nodeId }) => {
+      return queryClient.invalidateQueries({
+        queryKey: [queryKeys.CARD, nodeId],
+      });
+    },
+  });
+
+  const nodeId = useAppSelector(selectNodeId);
   const cardTrayOpen = useAppSelector(selectCardTrayOpen);
 
-  const handleAddCardToNode = () => {
+  const { data } = useGetCardTypes();
+  const cardTypes = data?.data;
+  const cardGroups = cardTypes?.reduce(
+    (acc, curr) => {
+      acc[curr.groupType] ??= [];
+      acc[curr.groupType].push(curr);
+      return acc;
+    },
+    {} as Record<GroupTypeEnum, CardType[]>,
+  );
+
+  const handleAddCardToNode = (id: string) => () => {
     dispatch(closeCardTray());
+    mutate({ cardTypeId: id, nodeId });
   };
 
-  return { cardTrayOpen, onAddCardToNode: handleAddCardToNode };
+  const mapGroupTypeToString = (groupType: string) => {
+    switch (groupType) {
+      case GroupTypeEnum.TRANSITION:
+        return 'Flow Logic';
+    }
+  };
+
+  return {
+    cardGroups,
+    cardTrayOpen,
+    mapGroupTypeToString,
+    onAddCardToNode: handleAddCardToNode,
+  };
 };
